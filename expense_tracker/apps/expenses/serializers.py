08706a1510497d4621481089e911fb9bae01afa8
Shidochiri <ruthwk2@gmail.com>
2025-12-13 16:24:59 +0300

    Bug Fixes in Category serializers

--- expense_tracker/apps/expenses/serializers.py
+++ expense_tracker/apps/expenses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Expenses
+from apps.categories.models import Category
 
 
 class ExpensesSerializer(serializers.Serializer):
@@ -7,13 +8,23 @@ class ExpensesSerializer(serializers.Serializer):
     user = serializers.CharField(source='user.username', read_only=True)
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     description = serializers.CharField()
-    category = serializers.CharField(source='category.name', read_only=True)
+    category = serializers.IntegerField(write_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
     date = serializers.DateField(read_only=True)
 
     def create(self, validated_data):
-        return Expenses.objects.create(**validated_data)
+        category_id = validated_data.pop('category')
+        category = Category.objects.get(id=category_id)
+        expense = Expenses.objects.create(
+            category=category,
+            **validated_data
+        )
+        return expense
 
     def update(self, instance, validated_data):
+        if 'category' in validated_data:
+            category_id = validated_data.pop('category')
+            instance.category = Category.objects.get(id=category_id)
         instance.amount = validated_data.get('amount', instance.amount)
         instance.description = validated_data.get('description', instance.description)
         instance.save()
