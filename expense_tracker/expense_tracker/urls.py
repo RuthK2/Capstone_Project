49594aca92dab45cff836a41cafbd5c268f5f549
Shidochiri <ruthwk2@gmail.com>
2025-12-13 16:07:51 +0300

    General updates

--- expense_tracker/expense_tracker/urls.py
+++ expense_tracker/expense_tracker/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.authentication.urls')),
+    path('api/categories/', include('apps.categories.urls')),
+    path('api/expenses/', include('apps.expenses.urls')),
 ]
