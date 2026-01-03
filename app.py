380f099207390e2c107b09ad75e32e40ed5c4865
Shidochiri <ruthwk2@gmail.com>
2026-01-03 17:04:48 +0300

    deployment set up

---
+++ app.py
@@ -0,0 +1,6 @@
+# Django WSGI application
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_tracker.expense_tracker.settings')
+application = get_wsgi_application()
